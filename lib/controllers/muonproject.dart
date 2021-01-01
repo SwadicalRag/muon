bfa94f8ac398405896d92e20b6c1372f3071d70c
swadical <swadical@lab.glua.team>
2021-01-01 23:27:50 +1030

    Add copy/paste

--- lib/controllers/muonproject.dart
+++ lib/controllers/muonproject.dart
@@ -23,6 +23,7 @@ class MuonProjectController extends GetxController {
   // other
   final selectedNotes = Map<MuonNoteController,bool>().obs;
   final playheadTime = 0.0.obs;
+  List<MuonNote> copiedNotes = [];
   
   // subdivision manager
   final currentSubdivision = 1.obs;
