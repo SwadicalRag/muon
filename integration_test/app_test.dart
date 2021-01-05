272a11a2958c5d62b9e3ea4644ec49915d035658
swadical <swadical@lab.glua.team>
2021-01-05 16:52:14 +1030

    Remove unused test code

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -20,17 +20,5 @@ void _testMain() {
 
     // Trigger a frame.
     await tester.pumpAndSettle();
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
